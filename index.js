0486c13238cd350323f086a8d2d1f6544b1676e4
jyang17 <robert861212@gmail.com>
2018-04-22 00:29:54 -0400

    trying to send html

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+var express = require('express');
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var bodyParser = require('body-parser');
+var validator = require('validator');
+var url = require("url");
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); // Required if we need to use HTTP post parameters
+
+var mongoUri = process.env.MONGODB_URI || process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/nodemongoexample';
+var MongoClient = require('mongodb').MongoClient, format = require('util').format;
+var db = MongoClient.connect(mongoUri, function(error, databaseConnection) {
+	db = databaseConnection;
+});
+
+var vehicles = ["JANET", "lGhCpJCE5K", "K9m65WRQyh", "3VaFZQS9Ee", "qrsXYLSLFw", 
+"TapqFEtdFF", "FwXkFfHWwT", "E1TmLM4UO6", "6VvCKigQ21", "YTBj8wiOXz", "KiztarwO7h", 
+"Ein4EIwThk", "5EmzPciOP1", "dPA7wAzZoe", "QMrHmCoyCE", "oRF2MrZv83", "DsRILKPCEf", "bomkcQM8oI"];
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', function(request, response) {
+	response.header("Access-Control-Allow-Origin", "*");
+   	response.header("Access-Control-Allow-Headers", "X-Requested-With");
+   	response.set('Content-Type', 'text/html');
+
+   	response.sendFile("index.html", {root:__dirname});
+});
+
+app.listen(process.env.PORT || 3000);
