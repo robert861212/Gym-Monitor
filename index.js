15ff8a7048d4132e0ddccfa681892067f4a5cf7f
jyang17 <robert861212@gmail.com>
2018-04-22 01:43:35 -0400

    changed image directory

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get('/', function(request, response) {
 	response.header("Access-Control-Allow-Origin", "*");
    	response.header("Access-Control-Allow-Headers", "X-Requested-With");
     response.set('Content-Type', 'text/html');
-   	response.sendFile("index.html", {root:__dirname});
+   	response.render("index.html", {root:__dirname});
    	//response.set('Content-Type', 'text/css');
    	//response.sendFile("style.css", {root:__dirname});
    	
