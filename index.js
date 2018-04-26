d873c582db73957361df192b1da1aef0f1c0813c
jyang17 <robert861212@gmail.com>
2018-04-26 12:51:45 -0400

    try dynamic graph

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ app.get('/script.js', function(request, response) {
             hourCount = results[i].number;
             fs.readFile("script.js", 'utf8', function (err,data) {
                replaceHour = "number" + currentHour;
-               result = data.replace(replaceHour, 1);
+               result = data.replace("number7", hourCount);
             });
          }
          response.send(result);
