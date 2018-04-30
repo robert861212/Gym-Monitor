e922edeb1ce1b4c1a27b5e36648fc9deb9859982
jyang17 <robert861212@gmail.com>
2018-04-30 15:36:54 -0400

    improved style

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ schedule.scheduleJob(rule, function(){
                         coll.insert(toInsertHr);
 
                            db.collection('days', function(error, collD) {
-                           var previous_entry_day = { "day": day.toString()};
+                           var previous_entry_day = { "day": day};
                            collD.remove(previous_entry_day, function(error, col) {
                               db.collection('days', function(error, collD) {
                                  collD.insert(toInsertDay, function(error, saved) {
